6ce1355540bcd9246102b9694a2602efb608e074
Michele Lisignoli <michele@lisignoli.com>
2021-08-30 00:05:13 +0200

    collection component and item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import HomePage from './components/pages/homepage/homepage.component';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import HomePage from './components/pages/homepage/homepage.component';
+import ShopPage from './components/shop/shop.component';
+
+
 
 function App() {
   return  (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route path="/hats" component={HatsPage}/>
+        <Route path="/shop" component={ShopPage}/>
       </Switch>
     </div>
   )
