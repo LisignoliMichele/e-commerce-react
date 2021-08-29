55a14d7e83b0a1586af6fa8beb726177b2b20e9d
Michele Lisignoli <michele@lisignoli.com>
2021-08-29 17:51:14 +0200

    HomePage Directory components

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../diractory/directory.component';
+import './homepage.stylesheet.scss';
+
+const HomePage = () => (
+   <div className='homepage'>
+      <Directory />
+   </div>
+)
+
+export default HomePage;
